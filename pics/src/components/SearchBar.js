90c9e078c24ce9fc0dec0e4888ee8823afa207db
Kenneth <kennethsbakke@gmail.com>
2021-09-20 15:11:41 -0700

    finished pics app using Ref

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,16 +1,29 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    
-  state = {};
+  state = {term: ''};
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  };
 
   render () {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image search</label>
-            <input type="text" autofocus require placeholder="Image search"/>
+            <input 
+              type="text" 
+              value={this.state.term} 
+              onChange={(e) => this.setState({term: e.target.value})} 
+              autoFocus 
+              autoComplete="true" 
+              require="true" 
+              placeholder="Image search"
+            />
           </div>
         </form>
       </div>
